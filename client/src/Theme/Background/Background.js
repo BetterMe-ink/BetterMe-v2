487c38558f6eadbc91d3dcb442f093919cf845df
leocrossman <lkcrossman@gmail.com>
2022-01-05 16:42:03 -0500

    progress on dark theme

---
+++ client/src/Theme/Background/Background.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Background.module.scss';
+const Background = ({ children }) => {
+  return <div className='.bg'>{children}</div>;
+};
+
+export default Background;
