95cdc88f364b9144c2d4d27af872199b0d9d0470
leocrossman <lkcrossman@gmail.com>
2022-01-06 18:22:52 -0500

    nav done

--- client/src/Components/ScrollToTop.js
+++ client/src/Components/ScrollToTop.js
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
 
-const ScrollToTop = (props) => {
+const ScrollToTop = props => {
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
