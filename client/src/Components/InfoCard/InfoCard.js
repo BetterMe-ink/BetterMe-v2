910c28d5e4d5655c9be15fb310cd968c8edecaa5
leocrossman <lkcrossman@gmail.com>
2022-01-06 18:13:19 -0500

    done with infocard

--- client/src/Components/InfoCard/InfoCard.js
+++ client/src/Components/InfoCard/InfoCard.js
@@ -1,4 +1,4 @@
-import style from './InfoCard.module.scss';
+import './InfoCard.scss';
 import Icon from '@mdi/react';
 
 import { mdiFoodForkDrink } from '@mdi/js';
@@ -12,30 +12,56 @@ import { mdiBottleTonicPlusOutline } from '@mdi/js';
 import { mdiWeightLifter } from '@mdi/js';
 
 function InfoCard({ data }) {
-
-    return (
-        <div className={style.Card}>
-
-            <div className={style.svgSection}>
-                <div className={`${style.svgDiv} ${data.idx === 1 ? style.searchDiv : data.idx === 2 ? style.gymDiv : data.idx === 3 ? style.healthDiv : data.idx === 4 ? style.plannerDiv : style.nutritionDiv}`}>
-
-                    <Icon path={`${data.idx === 1 ? mdiShieldSearch : data.idx === 2 ? mdiWeightLifter : data.idx === 3 ? mdiBottleTonicPlusOutline : data.idx === 4 ? mdiFoodForkDrink : mdiNutrition }`} 
-                    size={1.5}
-                    
-                    className={`${data.idx === 1 ? style.search : data.idx === 2 ? style.gym : data.idx === 3 ? style.health : data.idx === 4 ? style.planner : style.nutrition }`} 
-
-                    />
-
-                </div>
-            </div>
-
-            <div className={style.info}>
-                <h1>{data.title}</h1>
-                <p>{data.txt}</p>
-            </div>
-
+  return (
+    <div className={'InfoCard-Card'}>
+      <div className={'InfoCard-svgSection'}>
+        <div
+          className={`${'InfoCard-svgDiv'} ${
+            data.idx === 1
+              ? 'InfoCard-searchDiv'
+              : data.idx === 2
+              ? 'InfoCard-gymDiv'
+              : data.idx === 3
+              ? 'InfoCard-healthDiv'
+              : data.idx === 4
+              ? 'InfoCard-plannerDiv'
+              : 'InfoCard-nutritionDiv'
+          }`}
+        >
+          <Icon
+            path={`${
+              data.idx === 1
+                ? mdiShieldSearch
+                : data.idx === 2
+                ? mdiWeightLifter
+                : data.idx === 3
+                ? mdiBottleTonicPlusOutline
+                : data.idx === 4
+                ? mdiFoodForkDrink
+                : mdiNutrition
+            }`}
+            size={1.5}
+            className={`${
+              data.idx === 1
+                ? 'InfoCard-search'
+                : data.idx === 2
+                ? 'InfoCard-gym'
+                : data.idx === 3
+                ? 'InfoCard-health'
+                : data.idx === 4
+                ? 'InfoCard-planner'
+                : 'InfoCard-nutrition'
+            }`}
+          />
         </div>
-    )
+      </div>
+
+      <div className={'InfoCard-info'}>
+        <h1>{data.title}</h1>
+        <p>{data.txt}</p>
+      </div>
+    </div>
+  );
 }
 
-export default InfoCard
+export default InfoCard;
