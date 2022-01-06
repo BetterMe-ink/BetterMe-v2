1132f8fd0e5efe015b3cdc5d5806e13e5deda4a6
leocrossman <lkcrossman@gmail.com>
2022-01-06 18:05:01 -0500

    done with food item

--- client/src/Components/FoodItem/FoodItem.js
+++ client/src/Components/FoodItem/FoodItem.js
@@ -1,15 +1,20 @@
-import style from './FoodItem.module.scss';
+import './FoodItem.scss';
 import { useNavigate } from 'react-router-dom';
 
 function FoodItem(props) {
-    const navigate = useNavigate();
-    return (
-        <div className={style.main}>
-            <img className={style.img} src={props.image} alt="" />
-            <h1>{props.name}</h1>
-            <button className={style.btn} onClick={() => navigate(`/search/${props.id}`)}>See More</button>
-        </div>
-    )
+  const navigate = useNavigate();
+  return (
+    <div className={'FoodItem-main'}>
+      <img className={'FoodItem-img'} src={props.image} alt="" />
+      <h1>{props.name}</h1>
+      <button
+        className={'FoodItem-btn'}
+        onClick={() => navigate(`/search/${props.id}`)}
+      >
+        See More
+      </button>
+    </div>
+  );
 }
 
-export default FoodItem
+export default FoodItem;
